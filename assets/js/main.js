84196e10c72c47e924dab4b683b3139f669f4fa7
Chad Kruse <chadokruse@gmail.com>
2016-04-22 10:37:47 -0500

    Improve mobile popups [refs #2]

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,13 +138,11 @@
             caption: function($a) { return $a.next('h3').text(); },
             overlayColor: '#2c2c2c',
             overlayOpacity: 0.85,
-            popupCloserText: '',
-            popupLoaderText: '',
             selector: '.work-item a.image',
             usePopupCaption: true,
             usePopupDefaultStyling: false,
             usePopupEasyClose: false,
-            usePopupNav: true,
+            usePopupNav: false,
             windowMargin: (skel.breakpoint('small').active ? 0 : 50)
           });
 
